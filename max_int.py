a697cfb03661509a8fdc8a00a79651a5204f9462
armannelv <armannelv@gmail.com>
2020-09-03 10:39:31 +0000

    algorithm changes to max_int

--- max_int.py
+++ max_int.py
@@ -1,9 +1,7 @@
 num_int = 0
 max_int = 0
-high_int = 0
 while num_int >= 0:
     num_int = int(input("Input a number: "))  # Do not change this line
-    high_int = num_int
     if max_int < num_int:
         max_int = num_int
         continue
